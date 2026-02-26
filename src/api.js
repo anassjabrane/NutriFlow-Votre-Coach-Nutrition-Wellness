cbd206acdadb8d9717d43e2326a9bee88b76202c
Anass Jabrane <jabraneanass12@gmail.com>
2026-02-26 14:16:58 +0000

    ( Creation de logique Nutri-Badges

--- src/api.js
+++ src/api.js
@@ -1,37 +1,47 @@
 async function getRecipes(params) {
-    let x =await fetch('https://dummyjson.com/recipes');
-    let y = await x.json();
+  let x = await fetch("https://dummyjson.com/recipes");
+  let y = await x.json();
 
-    console.log(y);
-
+  console.log(y);
+  displayRecipes(y.recipes);
 }
-getRecipes();
-
+// displayRecipes(y.recipes);
 
 function displayRecipes(recipes) {
-    const app = document.getElementById("app");
+  const app = document.getElementById("app");
 
-    // Khwi l-container bach may-t-3awduch les cartes
-    app.innerHTML="";
+  // Khwi l-container bach may-t-3awduch les cartes
+  app.innerHTML = "";
 
-    for (let i=0; i<recipes.length; i++){
+  for (let i = 0; i < recipes.length; i++) {
+    // recette li fuha noba
 
-        // recette li fuha noba 
-        
-        let recipe = recipes[i];
-        
-        // ajouter la carte de html 
+    let recipe = recipes[i];
+    let badgeColor = "green";
 
-        const card =  `
-        <div class "recipes-card">
-             <img src="${recipe.img}" alt="§{recipe.name}">
+    if (recipe.caloriesPerServing < 400) {
+      badgeColor = "green";
+    } else if (
+      recipe.caloriesPerServing <= 800 &&
+      recipe.caloriesPerServing >= 400
+    ) {
+      badgeColor = "orange";
+    } else {
+      badgeColor = "red";
+    }
+
+    // ajouter la carte de html
+
+    const card = `
+        <div class="recipes-card">
+             <img src="${recipe.image}" alt="${recipe.image}">
             <h1>${recipe.name}</h1>
-            <p>${recipe.temps-preparation}</p>
+
+            <span class="nutre-badge">${badgeColor}<p>${recipe.prepTimeMinutes}Kcal</p></span>
 
         </div> `;
 
-        app.innerHTML=app.innerHTML + card;
-
-    }
+    app.innerHTML = app.innerHTML + card;
+  }
 }
-
+getRecipes();
