e8ae443a4280fb65271c6f5c56c27d32adc09fc8
Anass Jabrane <jabraneanass12@gmail.com>
2026-02-26 10:17:58 +0000

    l'affichages des recettes sur la page

--- src/api.js
+++ src/api.js
@@ -3,6 +3,35 @@ async function getRecipes(params) {
     let y = await x.json();
 
     console.log(y);
-}
 
+}
+getRecipes();
+
+
+function displayRecipes(recipes) {
+    const app = document.getElementById("app");
+
+    // Khwi l-container bach may-t-3awduch les cartes
+    app.innerHTML="";
+
+    for (let i=0; i<recipes.length; i++){
+
+        // recette li fuha noba 
+        
+        let recipe = recipes[i];
+        
+        // ajouter la carte de html 
+
+        const card =  `
+        <div class "recipes-card">
+             <img src="${recipe.img}" alt="§{recipe.name}">
+            <h1>${recipe.name}</h1>
+            <p>${recipe.temps-preparation}</p>
+
+        </div> `;
+
+        app.innerHTML=app.innerHTML + card;
+
+    }
+}
 
